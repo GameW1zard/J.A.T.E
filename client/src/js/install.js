97af27cbea0edb0d1d354b95e0b467f10611a6c9
GameW1zard <rivenloya@gmail.com>
2023-06-15 21:14:05 -0500

    init commit

---
+++ client/src/js/install.js
@@ -0,0 +1,35 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', function (event) {
+
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+    console.log('PWA not installed');
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async function () {
+
+        const promptEvent = window.deferredPrompt;
+
+        if (!promptEvent) {
+            return;
+        }
+
+        promptEvent.prompt();
+
+        window.deferredPrompt = null;
+
+        butInstall.classList.toggle('hidden', true);
+        console.log('PWA was installed');
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', function (event) {
+
+    window.deferredPrompt = null;
+    console.log('already installed');
+
+});
